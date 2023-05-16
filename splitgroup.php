7fe96f509268d30edadd484eac25ce5ae05466d9
Diego Muñio <muniodiego@gmail.com>
2023-05-16 11:06:25 -0300

    Update civix files.

--- splitgroup.php
+++ splitgroup.php
@@ -1,11 +1,14 @@
 <?php
 
 require_once 'splitgroup.civix.php';
+// phpcs:disable
+use CRM_Splitgroup_ExtensionUtil as E;
+// phpcs:enable
 
 /**
  * Implements hook_civicrm_config().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_config/
  */
 function splitgroup_civicrm_config(&$config) {
   _splitgroup_civix_civicrm_config($config);
@@ -14,9 +17,7 @@ function splitgroup_civicrm_config(&$config) {
 /**
  * Implements hook_civicrm_xmlMenu().
  *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
  */
 function splitgroup_civicrm_xmlMenu(&$files) {
   _splitgroup_civix_civicrm_xmlMenu($files);
@@ -25,16 +26,25 @@ function splitgroup_civicrm_xmlMenu(&$files) {
 /**
  * Implements hook_civicrm_install().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
  */
 function splitgroup_civicrm_install() {
   _splitgroup_civix_civicrm_install();
 }
 
 /**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
+ */
+function splitgroup_civicrm_postInstall() {
+  _splitgroup_civix_civicrm_postInstall();
+}
+
+/**
  * Implements hook_civicrm_uninstall().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
  */
 function splitgroup_civicrm_uninstall() {
   _splitgroup_civix_civicrm_uninstall();
@@ -43,7 +53,7 @@ function splitgroup_civicrm_uninstall() {
 /**
  * Implements hook_civicrm_enable().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function splitgroup_civicrm_enable() {
   _splitgroup_civix_civicrm_enable();
@@ -52,7 +62,7 @@ function splitgroup_civicrm_enable() {
 /**
  * Implements hook_civicrm_disable().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
  */
 function splitgroup_civicrm_disable() {
   _splitgroup_civix_civicrm_disable();
@@ -61,14 +71,7 @@ function splitgroup_civicrm_disable() {
 /**
  * Implements hook_civicrm_upgrade().
  *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
  */
 function splitgroup_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _splitgroup_civix_civicrm_upgrade($op, $queue);
@@ -80,7 +83,7 @@ function splitgroup_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
  * Generate a list of entities to create/deactivate/delete when this module
  * is installed, disabled, uninstalled.
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
  */
 function splitgroup_civicrm_managed(&$entities) {
   _splitgroup_civix_civicrm_managed($entities);
@@ -91,11 +94,9 @@ function splitgroup_civicrm_managed(&$entities) {
  *
  * Generate a list of case-types.
  *
- * @param array $caseTypes
- *
  * Note: This hook only runs in CiviCRM 4.4+.
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
  */
 function splitgroup_civicrm_caseTypes(&$caseTypes) {
   _splitgroup_civix_civicrm_caseTypes($caseTypes);
@@ -109,33 +110,49 @@ function splitgroup_civicrm_caseTypes(&$caseTypes) {
  * Note: This hook only runs in CiviCRM 4.5+. It may
  * use features only available in v4.6+.
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
  */
 function splitgroup_civicrm_angularModules(&$angularModules) {
-_splitgroup_civix_civicrm_angularModules($angularModules);
+  _splitgroup_civix_civicrm_angularModules($angularModules);
 }
 
 /**
  * Implements hook_civicrm_alterSettingsFolders().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
  */
 function splitgroup_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
   _splitgroup_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
- * Functions below this ship commented out. Uncomment as required.
+ * Implements hook_civicrm_entityTypes().
  *
+ * Declare entity types provided by this module.
+ *
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+ */
+function splitgroup_civicrm_entityTypes(&$entityTypes) {
+  _splitgroup_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_themes().
+ */
+function splitgroup_civicrm_themes(&$themes) {
+  _splitgroup_civix_civicrm_themes($themes);
+}
+
+// --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
  * Implements hook_civicrm_preProcess().
  *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
- *
-function splitgroup_civicrm_preProcess($formName, &$form) {
-
-} // */
+ * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
+ */
+//function splitgroup_civicrm_preProcess($formName, &$form) {
+//
+//}
 
 /**
  * Implements hook_civicrm_navigationMenu().
